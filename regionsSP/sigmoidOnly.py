974f488ebe1320aa14040d5cc4176dbd3498173c
gasilva <gasilva@ats4i.com.br>
2020-07-26 19:15:45 -0300

    update RegionsSP

---
+++ regionsSP/sigmoidOnly.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+from numba import njit
+import math
+import numpy as np
+
+@lru_cache(maxsize=None)
+@njit #(parallel=True)
+def sigmoid(x,betai,betaf):
+    if betai>betaf:
+        return 1-(1 / (1 + math.exp(-x)))
+    else:
+        return 1 / (1 + math.exp(-x))
+        
